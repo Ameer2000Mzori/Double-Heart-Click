c723390134c5e089e4cdd0c6ff0b275333b6895c
Ameer <ameerapex496@gmail.com>
2023-12-06 05:17:18 +0100

    added styling added function and time outs

--- src/script.js
+++ src/script.js
@@ -1,7 +1,21 @@
 // selecting elements
 var imageEl = document.getElementsByClassName("image-wrap")[0];
+var Body = document.querySelector("body");
 // functions
 // event lisnters
 imageEl.addEventListener("dblclick", function () {
-    console.log("clickedX2");
+    console.log("double Clicked");
+    var heartEl = document.createElement("img");
+    heartEl.src = "./assets/heart-png-15.png";
+    heartEl.classList.add("heart-Img");
+    setTimeout(function () {
+        heartEl.classList.add("active");
+    }, 500);
+    setTimeout(function () {
+        heartEl.classList.remove("active");
+        setTimeout(function () {
+            heartEl.remove();
+        }, 1100);
+    }, 1100);
+    Body === null || Body === void 0 ? void 0 : Body.appendChild(heartEl);
 });
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,24 @@
 // selecting elements
 const imageEl = document.getElementsByClassName("image-wrap")[0];
-
+const Body = document.querySelector("body");
 // functions
 
 // event lisnters
 imageEl.addEventListener("dblclick", () => {
-  console.log("clickedX2");
+  console.log("double Clicked");
+  const heartEl = document.createElement("img");
+  heartEl.src = "./assets/heart-png-15.png";
+  heartEl.classList.add("heart-Img");
+  setTimeout(() => {
+    heartEl.classList.add("active");
+  }, 500);
+
+  setTimeout(() => {
+    heartEl.classList.remove("active");
+    setTimeout(() => {
+      heartEl.remove();
+    }, 1100);
+  }, 1100);
+
+  Body?.appendChild(heartEl);
 });
